6084c70d84522087775d7e2dc1fddaf4a66fca55
Evan Senter <evansenter@gmail.com>
2012-07-29 17:24:02 +0200

    Saving this before I try to hack it apart to support lists across multiple lines.

--- parser.rb
+++ parser.rb
@@ -58,22 +58,57 @@ require "parslet"
 # root :expression
 
 class DinoParser < Parslet::Parser
+  # White-space
   rule(:terminate) { (non_terminating_space? >> end_of_line >> non_terminating_space?).repeat(1) }
-  rule(:end_of_line) { match("\n").repeat(1) }
-  rule(:end_of_line?) { end_of_line.maybe }
+  rule(:end_of_line) { match("\n") }
   rule(:non_terminating_space) { match[" \t"].repeat(1) }
   rule(:non_terminating_space?) { non_terminating_space.maybe }
   
-  rule(:integer) { match["0-9"].repeat(1).as(:integer) >> non_terminating_space? }
-  rule(:atom) { integer }
+  # Simple
+  rule(:characters) { match["A-Za-z"].repeat(1) }
+  rule(:characters?) { characters.maybe }
   
-  rule(:expression) { non_terminating_space? >> (str("exp") | atom).as(:expression) }
+  # Numerics
+  rule(:sign?) { match["+-"].maybe.as(:sign) }
+  rule(:digits) { match["0-9"].repeat(1) }
+  rule(:digits?) { digits.maybe }
+  rule(:significand) { (digits? >> str(".") >> digits).as(:significand) }
+  rule(:exponent?) { (match["eE"] >> digits.as(:exponent)).maybe }
+  rule(:integer) { sign? >> digits.as(:value) >> non_terminating_space? }
+  rule(:float) { (sign? >> significand >> exponent?).as(:value) >> non_terminating_space? }
+  rule(:number) { (float.as(:float) | integer.as(:integer)) >> non_terminating_space? }
+  
+  # Strings (still needs interpolation)
+  rule(:string) { (str(?") >> match['^"'].repeat.as(:body) >> str(?")).as(:string) >> non_terminating_space? }
+  
+  # Grammar
+  rule(:atom) { number | string }
+  rule(:function_name) { (characters >> (digits | characters).repeat >> str(??).maybe).as(:name) }
+  rule(:function_call) { (function_name).as(:function_call) >> non_terminating_space? }
+  rule(:list) { (str(?() >> str(?))).as(:list) >> non_terminating_space? }
+  rule(:expression) { non_terminating_space? >> (function_call | atom).as(:expression) }
   rule(:expressions) { ((expression >> terminate).repeat >> expression.maybe).as(:expressions) }
 
   root(:expressions)
 end
 
 pp DinoParser.new.parse("")
-pp DinoParser.new.parse("  \t12\n\n  ")
+pp DinoParser.new.parse("\"\"")
+pp DinoParser.new.parse("\"\"\n\"\"")
+pp DinoParser.new.parse('"12345"')
+pp DinoParser.new.parse("10\n+10\n-10")
+pp DinoParser.new.parse("10.0\n+.10\n-1.0e10")
 pp DinoParser.new.parse("exp\nexp\nexp\n\nexp\n\n\n")
-pp DinoParser.new.parse("exp\nexp\nexp\n\nexp")
+pp DinoParser.new.parse("exp\nexp10?\nexp\n\nexp")
+
+string = <<-STR
+(class Foo
+ (function bar [name] (
+   puts name
+ ))
+)
+
+(bar (new Foo) "Evan")
+STR
+
+pp DinoParser.new.parse(string)
